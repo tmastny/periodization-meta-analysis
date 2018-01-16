9201af2c793d27349c4dba785f22700cbb4a6484
Timothy Mastny <tim.mastny@gmail.com>
2018-01-16 14:59:46 -0600

    added read_chunk markup

--- tidier.R
+++ tidier.R
@@ -1,4 +1,5 @@
 
+## ---- spatial ----
 library(tidyverse)
 library(magrittr)
 d <- readxl::read_excel('Periodization Stuff.xlsx')
@@ -9,16 +10,17 @@ for (i in 1:nrow(d)) {
   }
 }
 
-# remove URL
+## ---- remove_url ----
 d$`Other 1 pre` <- as.numeric(d$`Other 1 pre`)
 
-d %<>%
-  select(
-    -`Measurements at 3+ time points?`, -Author, -`Study Title`,
-    -`Participants (training status)`, -Age, -Sex, -`Length (weeks)`,
-    -`Intensity Closest to 1RM test`, -`Volume Equated?`, -Issues)
+## ---- declutter ----
+# d %<>%
+#   select(
+#     -`Measurements at 3+ time points?`, -Author, -`Study Title`,
+#     -`Participants (training status)`, -Age, -Sex, -`Length (weeks)`,
+#     -`Intensity Closest to 1RM test`, -`Volume Equated?`, -Issues)
 
-# add in missing values
+## ---- gather_variables ----
 d %<>%
   gather(type, outcome, `LBM Pre`:`ES 1 vs. 3__4`) %>%
   mutate(type = str_to_lower(type)) %>%
@@ -31,18 +33,19 @@ d %<>%
       type, c('outcome_type', 'outcome_status'), 
       ' (?=(pre$|post$|sd$))')
 
+## ---- remove_na -----
 d <- d[complete.cases(d$outcome),]
 d$outcome <- as.numeric(d$outcome)
 
-d %<>% #add outcome_status to this grouping as well
+## ---- group_by -----
+d %<>% 
   mutate_if(is.character, funs(factor(.))) %>%
   group_by(
     outcome_type, outcome_status, Number, `Program Label`, `Program Details`) %>%
   mutate(i = row_number()) %>%
   spread(outcome_status, outcome) %>%
-  ungroup()
+  ungroup() %>%
+  select(-i)
 
 
-d %>%
-  filter(Number == 1) %>%
-  select(-`Program Details`)
+
